c3823e529a095b02b4bac4379a377352910fcd66
一方通行 <SaltzmanAlaric@github.com>
2020-10-22 12:20:57 +0800

    reids整合

---
+++ common/src/main/java/com/study/common/redis/RedisConfig.java
@@ -0,0 +1,83 @@
+package com.study.common.redis;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.*;
+
+import java.io.Serializable;
+import java.net.UnknownHostException;
+import java.time.Duration;
+
+
+@Configuration
+@AutoConfigureAfter(RedisAutoConfiguration.class)
+public class RedisConfig {
+
+    /**
+     * jackJSON转换对象
+     */
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory)
+    {
+        RedisTemplate<String,Object> template = new RedisTemplate<String,Object>();
+        template.setConnectionFactory(redisConnectionFactory);
+        //使用JSON格式的序列化,保存
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    //自定义cacheManager缓存管理器
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory factory)
+    {
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        //解决查询缓存转换异常的问题
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        //配置序列化(解决乱码的问题)
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ZERO)
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
+                .disableCachingNullValues();
+
+        RedisCacheManager cacheManager = RedisCacheManager.builder(factory)
+                .cacheDefaults(config)
+                .build();
+        return cacheManager;
+    }
+}
--- consumer/src/main/java/com/study/controller/TestController.java
+++ consumer/src/main/java/com/study/controller/TestController.java
@@ -2,11 +2,15 @@ package com.study.controller;
 
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
 import com.github.xiaoymin.knife4j.annotations.ApiSort;
+import com.study.common.redis.RedisUtils;
 import com.study.common.web.Result;
 import com.study.provider.CityFeign;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +24,8 @@ public class TestController {
 
     @Resource
     private CityFeign cityFeign;
+    @Resource
+    private RedisUtils redisUtils;
 
     @ApiOperationSupport(order = 1, author = "test")
     @ApiOperation("获取城市信息feign")
@@ -43,4 +49,13 @@ public class TestController {
         return Result.success("获取操作系统名称成功", system);
     }
 
+    @ApiOperationSupport(order = 4, author = "test")
+    @ApiOperation("获取redis")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "key", value = "键", required = true, dataType = "String", paramType = "path", example = "id")
+    })
+    @GetMapping("redis/{key}")
+    public Result getRedis(@PathVariable("key")String key){
+        return Result.success("获取redis键的值成功", redisUtils.get(key));
+    }
 }
